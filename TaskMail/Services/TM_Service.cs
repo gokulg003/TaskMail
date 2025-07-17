def68cf3afe1f8cd0d293897080f0c1f43a747cc
Devi <dsuresh@simplesolve.com>
2025-07-17 11:47:41 +0530

    Cleaned up ignored files

--- TaskMail/Services/TM_Service.cs
+++ TaskMail/Services/TM_Service.cs
@@ -2,10 +2,7 @@ using System.Data;
 using System.Data.SqlClient;
 using AutoMapper;
 using Dapper;
-using Microsoft.VisualBasic;
 using TaskMail.Services.common;
-
-
 
 namespace TaskMail.Services.Services
 {
@@ -13,41 +10,46 @@ namespace TaskMail.Services.Services
     {
         private readonly IConfiguration _config;
         private readonly IMapper _mapper;
+
         public TaskMail_Service(IConfiguration config, IMapper mapper)
         {
             _config = config;
             _mapper = mapper;
         }
+
         public IDbConnection GetConnection
         {
             get { return new SqlConnection(_config.GetConnectionString("DefaultConnection")); }
         }
 
-        public TaskMail_Login_VM Login(TaskMail_Login_VM loginVm, in string Username, in string Password)
+        public TaskMail_Login_VM Login(TaskMail_Login_VM loginVm)
         {
             try
             {
                 using (IDbConnection conn = GetConnection)
                 {
                     conn.Open();
-                    var param = new DynamicParameters();
-                    param.Add(Constant.UserName, dbType: DbType.String, size: 200, direction: ParameterDirection.Input);
-                    param.Add(Constant.Password, dbType: DbType.String, size: 200, direction: ParameterDirection.Input);
-                    param.Add(Constant.errmsglogin, dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
-                    var model = conn.Query<TaskMail_Login_DM>(Constant.Login_SP, param, commandType: CommandType.StoredProcedure);
-                    Username = param.Get<Int16>(Constants.UserName);
-                    Password = param.Get<string>(Constants.Password);
-                    return errmsglogin;
-                   
-                }
-                
-            }
-            catch
-            {
 
+                    var param = new DynamicParameters();
+                    param.Add(Constant.UserName, loginVm.Username, DbType.String, ParameterDirection.Input, 200);
+                    param.Add(Constant.Password, loginVm.Password, DbType.String, ParameterDirection.Input, 200);
+                    param.Add(Constant.errmsglogin, dbType: DbType.String, size: 200, direction: ParameterDirection.Output);
+                    var result = conn.Query<TaskMail_Login_DM>(Constant.Login_SP, param, commandType: CommandType.StoredProcedure).FirstOrDefault();
+                    string errmsg = param.Get<string>(Constant.errmsglogin);
+
+                    if (!string.IsNullOrEmpty(errmsg))
+                    {
+                        throw new Exception(errmsg); 
+                    }
+
+                    var viewModel = _mapper.Map<TaskMail_Login_VM>(result);
+                    return viewModel;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Login failed: " + ex.Message);
             }
         }
     }
-
-
 }
