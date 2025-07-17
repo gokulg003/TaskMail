f39072bee3492942004a3c04e7feaff78842308d
Gokul G <ggurumurthy@simplesolve.com>
2025-07-17 13:08:07 +0530

    Changed

--- TaskMail/ViewModels/TM_Login_VM.cs
+++ TaskMail/ViewModels/TM_Login_VM.cs
@@ -1,7 +1,11 @@
+using System.Text.Json.Serialization;
+
 namespace TaskMail.ViewModels;
+
 public class TaskMail_Login_VM
 {
     public string UserName { get; set; }
     public string Password { get; set; }
-    public string? Message { get; set; } 
+   [JsonIgnore]
+    public string? Message { get; set; }
 }
