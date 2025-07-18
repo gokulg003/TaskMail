255b0dce5250597dd3b02be901e9b01431e65730
Gokul G <ggurumurthy@simplesolve.com>
2025-07-18 15:56:07 +0530

    Grid 1 inital

---
+++ TaskMail/DataModels/TaskMail_Template_Time.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace TaskMail.ViewModels;
+
+public class TaskMail_Template_Time_DM
+{
+    public string TM_Template_Time_PK { get; set; }
+    public string TM_Type { get; set; }
+    public string TM_Month { get; set; }
+    public string TM_Date { get; set; }
+    public string TM_Year { get; set; }
+    public string TM_In_Time { get; set; }
+    public string TM_Out_Time { get; set; }
+    public string TM_Total_Duration { get; set; }
+    public string TM_Break_Duration  { get; set; }
+    public string TM_Act_Work_Hours{ get; set; }
+    public string? TM_Comments { get; set; }
+}
+
---
+++ TaskMail/ViewModels/TaskMail_Template_Time_VM.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace TaskMail.ViewModels;
+
+public class TaskMail_Template_Time_VM
+{
+    public string Resource { get; set; }
+    public string Type { get; set; }
+    public string Month { get; set; }
+    public string Date { get; set; }
+    public string Year { get; set; }
+    public string In_Time { get; set; }
+    public string Out_Time { get; set; }
+    public string Total_Duration { get; set; }
+    public string Break_Duration  { get; set; }
+    public string Act_Work_Hours{ get; set; }
+    public string? Comments { get; set; }
+}
+
