bd990998265f0a2da25a2f2a90a680fb43d589c5
Gokul G <ggurumurthy@simplesolve.com>
2025-07-21 15:17:57 +0530

    final grid 1

--- TaskMail/Services/ITaskMailTemplate_TimeService.cs
+++ TaskMail/Services/ITaskMailTemplate_TimeService.cs
@@ -5,6 +5,5 @@ namespace TaskMailService.Services
     public interface ITaskMailTemplateTime_Service
     {
         List<TaskMail_Template_Time_VM> Template_Time(TaskMail_Template_Time_VM TemplateTime_VM, TemplateTimeSupplements templateTimeSupplements);
-        object Template_Time(TemplateTimeSupplements templateTimeSupplements);
     }
 }
