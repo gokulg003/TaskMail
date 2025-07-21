fb837c6a9f18772c5c6406acbe45ea8c21c95e04
Devi <dsuresh@simplesolve.com>
2025-07-21 13:04:50 +0530

    changed

--- TaskMail/Services/ITaskMailTemplate_TimeService.cs
+++ TaskMail/Services/ITaskMailTemplate_TimeService.cs
@@ -1,9 +1,11 @@
-// using TaskMail.ViewModels;
+using TaskMail.ViewModels;
 
-// namespace TaskMailService.Services
-// {
-//     public interface ITaskMailTemplateTime_Service
-//     {
-//         TaskMail_Template_Time_VM Template_Time(TaskMail_Template_Time_VM TemplateTime_VM);
-//     }
-// }
+namespace TaskMailService.Services
+{
+    public interface ITaskMailTemplateTime_Service
+    {
+        List<TaskMail_Template_Time_VM> Template_Time(TaskMail_Template_Time_VM TemplateTime_VM, in string TM_Template_Time_PK,
+         in string TM_Type, in string TM_Month, in string TM_Date, in string TM_Year, in string TM_In_Time, in string TM_Out_Time,
+         in string TM_Total_Duration, in string TM_Break_Duration, in string TM_Act_Work_Hours, in string TM_Comments);
+    }
+}
--- TaskMail/Services/TaskMailTemplate_TimeService.cs
+++ TaskMail/Services/TaskMailTemplate_TimeService.cs
@@ -1,62 +1,64 @@
-// using System.Data;
-// using System.Data.SqlClient;
-// using AutoMapper;
-// using Dapper;
-// using TaskMail.DataModels;
-// using TaskMail.Services.common;
-// using TaskMail.ViewModels;
+using System.Data;
+using System.Data.SqlClient;
+using AutoMapper;
+using Dapper;
+using TaskMail.DataModels;
+using TaskMail.Services.common;
+using TaskMail.ViewModels;
 
-// namespace TaskMailService.Services
-// {
-//     public class TaskMailTemplateTime_Service : ITaskMailTemplateTime_Service
-//     {
-//         private readonly IConfiguration _config;
-//         private readonly IMapper _mapper;
+namespace TaskMailService.Services
+{
+    public class TaskMailTemplateTime_Service : ITaskMailTemplateTime_Service
+    {
+        private readonly IConfiguration _config;
+        private readonly IMapper _mapper;
 
-//         public TaskMailTemplateTime_Service(IConfiguration config, IMapper mapper)
-//         {
-//             _config = config;
-//             _mapper = mapper;
-//         }
+        public TaskMailTemplateTime_Service(IConfiguration config, IMapper mapper)
+        {
+            _config = config;
+            _mapper = mapper;
+        }
 
-//         public IDbConnection Connection
-//         {
-//             get
-//             {
-//                 return new SqlConnection(_config.GetConnectionString(Constant.databaseName));
-//             }
-//         }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString(Constant.databaseName));
+            }
+        }
 
-//         public List<TaskMail_Template_Time_DM> Template_Time()
-//         {
-//             var templatetimedm = new List<TaskMail_Template_Time_DM>();
-//             try
-//             {
-//                 using (IDbConnection con = Connection)
-//                 {
-//                     con.Open();
-//                     var parameters = new DynamicParameters();
-//                     parameters.Add(Constant.Resource,TM_Template_Time_PK , DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Type, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Month, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Date, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Year, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.In_Time, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Out_Time, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Total_Duration, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Break_Duration, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     parameters.Add(Constant.Act_Work_Hours, policyId, DbType.Int64, ParameterDirection.Input, 18); 
-//                     parameters.Add(Constant.Comments, policyId, DbType.Int64, ParameterDirection.Input, 18);
-//                     templatetimedm = con.Query<TaskMail_Template_Time_DM>(Constant.TemplateTime_SP, parameters, commandType: CommandType.StoredProcedure).ToList();
-//                 }
-//             }
-//             catch (Exception ex)
-//             {
-//                 loginVm.Message = "Login failed: " + ex.Message;
-//                 return loginVm; 
-//             }
-//         }
+        public List<TaskMail_Template_Time_VM> Template_Time(TaskMail_Template_Time_VM TemplateTime_VM,in string TM_Template_Time_PK,
+        in string TM_Type, in string TM_Month,in string TM_Date, in string TM_Year, in string TM_In_Time, in string TM_Out_Time,
+        in string TM_Total_Duration, in string TM_Break_Duration, in string TM_Act_Work_Hours, in string TM_Comments)
+        {
+            var templatetimedm = new List<TaskMail_Template_Time_DM>();
+            try
+            {
+                using (IDbConnection con = Connection)
+                {
+                    con.Open();
+                    var parameters = new DynamicParameters();
+                    parameters.Add(Constant.Resource,TM_Template_Time_PK , DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Type, TM_Type, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Month, TM_Month, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Date, TM_Date, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Year, TM_Year, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.In_Time, TM_In_Time, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Out_Time, TM_Out_Time, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Total_Duration, TM_Total_Duration, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Break_Duration, TM_Break_Duration, DbType.Int64, ParameterDirection.Input, 18);
+                    parameters.Add(Constant.Act_Work_Hours, TM_Act_Work_Hours, DbType.Int64, ParameterDirection.Input, 18); 
+                    parameters.Add(Constant.Comments, TM_Comments, DbType.Int64, ParameterDirection.Input, 18);
+                    templatetimedm = con.Query<TaskMail_Template_Time_DM>(Constant.TemplateTime_SP, parameters, commandType: CommandType.StoredProcedure).ToList();
+                }
+            }
+            catch (Exception ex)
+            {
+                loginVm.Message = "Login failed: " + ex.Message;
+                return loginVm; 
+            }
+        }
 
 
-//     }
-// }
+    }
+}
