d8112f7201d0104b4aaac795482d2a4a8c6375f8
Devi <dsuresh@simplesolve.com>
2025-07-24 14:04:31 +0530

    changed

--- TaskMail/Program.cs
+++ TaskMail/Program.cs
@@ -9,8 +9,8 @@ builder.Services.AddScoped<ITM_TaskHeaderService, TM_TaskHeaderService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddAutoMapper(typeof(MappingProfile));
-builder.Services.AddAutoMapper(typeof(TM_TaskHeaderMapping));
+builder.Services.AddAutoMapper(typeof(MappingProfile),typeof(TM_TaskHeaderMapping));
+// builder.Services.AddAutoMapper(typeof(TM_TaskHeaderMapping));
 
 
 
@@ -19,8 +19,8 @@ var app = builder.Build();
     app.UseSwagger();
     app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
+// app.UseHttpsRedirection();
+// app.UseAuthentication();
+// app.UseAuthorization();
 app.MapControllers();
 app.Run();
