a4152c97b0d4d3798434e3efe98957a7b5b929f9
bdhayalan <bdhayalan@LP012>
2025-07-25 14:36:56 +0530

    changes 2

---
+++ TaskMail/Mapping/UserLogin.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using TaskMail.DataModels;
+using TaskMail.ViewModels;
+
+namespace TaskMail.Mapping
+{
+    public class UserLogin:Profile
+    {
+        public UserLogin()
+        {
+            CreateMap<UserDetailsDM, UserDetails>() .ForMember(dest => dest.UsersId, opt => opt.MapFrom(src => src.UsersPk))
+                                                    .ReverseMap();
+        }
+    }
+}
