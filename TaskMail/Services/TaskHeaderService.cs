caea933a5d7ac3bcc6e3e3f7a0925f1d65790ce1
Gokul G <ggurumurthy@simplesolve.com>
2025-08-13 12:55:02 +0530

    Httpcontextacessor removed and using body param

--- TaskMail/Services/TaskHeaderService.cs
+++ TaskMail/Services/TaskHeaderService.cs
@@ -12,7 +12,7 @@ namespace TaskMailService.Services
     {
         private readonly IConfiguration _config;
         private readonly IMapper _mapper;
-        private readonly IHttpContextAccessor _httpContextAccessor;
+        // private readonly IHttpContextAccessor _httpContextAccessor;
 
         public TaskHeaderService(IConfiguration config, IMapper mapper)
         {
@@ -39,12 +39,16 @@ namespace TaskMailService.Services
                 {
                     con.Open();
                     var parameters = new DynamicParameters();
+
                     parameters.Add(ConstantDetails.Fromdate, fromDate, DbType.String, ParameterDirection.Input, 15);
                     parameters.Add(ConstantDetails.Todate, toDate, DbType.String, ParameterDirection.Input, 15);
                     parameters.Add(ConstantDetails.UserName, userName, DbType.String, ParameterDirection.Input, 250);
+
                     parameters.Add(ConstantDetails.dbparamstatus, dbType: DbType.Int16, direction: ParameterDirection.Output);
                     parameters.Add(ConstantDetails.dbparamerrmsg, dbType: DbType.String, direction: ParameterDirection.Output, size: 5000);
+
                     result = con.Query<TaskHeaderDM>(ConstantDetails.TaskHeader_RetrieveSP, parameters, commandType: CommandType.StoredProcedure).ToList();
+
                     status = parameters.Get<Int16>(ConstantDetails.status);
                     message = parameters.Get<string>(ConstantDetails.errMsg);
                 }
@@ -83,13 +87,13 @@ namespace TaskMailService.Services
 
                     parameters.Add(ConstantDetails.dbparamstatus, dbType: DbType.Int16, direction: ParameterDirection.Output, size: 1);
                     parameters.Add(ConstantDetails.dbparamerrmsg, dbType: DbType.String, direction: ParameterDirection.Output, size: 5000);
-                    parameters.Add(ConstantDetails.dbparamHeaderPk, dbType: DbType.Int16, direction: ParameterDirection.Output, size: 18);
+                    parameters.Add(ConstantDetails.dbparamHeaderPk, dbType: DbType.Int32, direction: ParameterDirection.Output, size: 18);
 
                     result = con.Query<TaskHeaderDM>(ConstantDetails.TaskHeader_SP, parameters, commandType: CommandType.StoredProcedure).FirstOrDefault();
 
                     status = parameters.Get<Int16>(ConstantDetails.status);
                     message = parameters.Get<string>(ConstantDetails.errMsg);
-                    headerId = parameters.Get<Int16>(ConstantDetails.HeaderPk);
+                    headerId = parameters.Get<Int32>(ConstantDetails.HeaderPk);
                 }
             }
             catch (Exception ex)
@@ -128,13 +132,13 @@ namespace TaskMailService.Services
 
                     parameters.Add(ConstantDetails.dbparamstatus, dbType: DbType.Int16, direction: ParameterDirection.Output, size: 1);
                     parameters.Add(ConstantDetails.dbparamerrmsg, dbType: DbType.String, direction: ParameterDirection.Output, size: 5000);
-                    parameters.Add(ConstantDetails.dbparamHeaderPk, dbType: DbType.Int16, direction: ParameterDirection.Output, size: 18);
+                    parameters.Add(ConstantDetails.dbparamHeaderPk, dbType: DbType.Int32, direction: ParameterDirection.Output, size: 18);
 
                     result = con.Query<TaskHeaderDM>(ConstantDetails.TaskHeaderUpdate_SP, parameters, commandType: CommandType.StoredProcedure).FirstOrDefault();
 
                     status = parameters.Get<Int16>(ConstantDetails.status);
                     message = parameters.Get<string>(ConstantDetails.errMsg);
-                    outHeaderId = parameters.Get<Int16>(ConstantDetails.HeaderPk);
+                    outHeaderId = parameters.Get<Int32>(ConstantDetails.HeaderPk);
                 }
             }
             catch (Exception ex)
