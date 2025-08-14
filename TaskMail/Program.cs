56acda45f46fdd8baeb4a675b51a1dbb107c8bbc
Gokul G <ggurumurthy@simplesolve.com>
2025-08-14 15:25:08 +0530

    CORS Error changes

--- TaskMail/Program.cs
+++ TaskMail/Program.cs
@@ -1,14 +1,14 @@
 using TaskMailService.Services;
-
-
+ 
+ 
 var builder = WebApplication.CreateBuilder(args);
-
+ 
 var configuration = builder.Configuration;
-
+ 
 builder.Services.AddHttpContextAccessor();
-
+ 
 builder.Services.AddSingleton<IConfiguration>(configuration);
-
+ 
 builder.Services.AddScoped<ILoginService, LoginService>();
 builder.Services.AddScoped<ITaskHeaderService, TaskHeaderService>();
 builder.Services.AddScoped<ICodeMasterService, CodeMasterService>();
@@ -18,32 +18,42 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-
+ 
+ 
 var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
+ 
 builder.Services.AddCors(options =>
 {
-    options.AddDefaultPolicy(policy =>
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
     {
-        policy.WithOrigins("MyAllowSpecificOrigins") // or whatever your frontend is
-              .AllowCredentials()
+        policy.WithOrigins("*")
+              .AllowAnyHeader()
               .AllowAnyMethod()
-              .AllowAnyHeader();
+              .AllowCredentials();
     });
 });
-var app = builder.Build();
-
  
-
+var app = builder.Build();
+ 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-// app.UseHttpsRedirection();
-// app.UseAuthentication();
-// app.UseAuthorization();
-
+app.UseRouting();
+ 
+// CORS
+app.UseCors(MyAllowSpecificOrigins);
+ 
+// Handle OPTIONS requests for CORS
+app.Use(async (context, next) =>
+{
+    if (context.Request.Method == "OPTIONS")
+    {
+        context.Response.StatusCode = 204;
+        return;
+    }
+    await next();
+});
+app.UseAuthorization();
+ 
 app.MapControllers();
-app.UseCors();
+ 
 app.Run();
-
-
