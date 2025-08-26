20bbf4ae4d1ae790d5de270a5c0704022895d93d
Gokul G <ggurumurthy@simplesolve.com>
2025-08-26 12:34:36 +0530

    MailCount added

--- TaskMail/ViewModels/TaskHeader.cs
+++ TaskMail/ViewModels/TaskHeader.cs
@@ -24,6 +24,6 @@ public class TaskHeader
 
     public string ResourceCode { get; set; }
     public string TypeCode { get; set; }
-    public string MailCount{ get; set; }
+    public long MailCount{ get; set; }
 }
 
